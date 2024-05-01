37476b1344314faa656d14fc6430719c32b0b93e
Viacheslav "Marpa3D" Sharov <Marpa3d@yandex.ru>
2024-05-01 04:44:19 +0300

    Создана функция добавления студента к списку студентов

--- structsAndAlgo/main.go
+++ structsAndAlgo/main.go
@@ -1,6 +1,21 @@
 // Generics
 package main
 
+import (
+	"fmt"
+)
+
+func addStudent(students []string, student string) []string {
+	return append(students, student)
+}
+
 func main() {
+
+	students := []string{} // Пустой срез
+	result := addStudent(students, "Марк")
+	result = addStudent(result, "Андрей")
+	result = addStudent(result, "Михаил")
+
+	fmt.Println(result)
 
 }
